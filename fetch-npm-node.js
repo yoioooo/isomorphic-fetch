eeba41ba28f0f6394cbfd673fe60abc04ed06050
yoooooo <pyooboom@gmail.com>
2024-01-11 01:46:10 +0800

    Update fetch-npm-node.js

--- fetch-npm-node.js
+++ fetch-npm-node.js
@@ -1,11 +1,14 @@
 "use strict";
 
 var realFetch = require('node-fetch');
+const { HttpsProxyAgent } = require('https-proxy-agent');
+
 module.exports = function(url, options) {
 	if (/^\/\//.test(url)) {
 		url = 'https:' + url;
 	}
-	return realFetch.call(this, url, options);
+	console.log('111', options);
+	return realFetch.call(this, url, {...options, agent: new HttpsProxyAgent('http://127.0.0.1:6152', { rejectUnauthorized: false })});
 };
 
 if (!global.fetch) {
